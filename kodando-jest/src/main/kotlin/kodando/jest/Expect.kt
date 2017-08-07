e7a3d216866b29621ddeb4dfd13621950248e775
Daniel Ferreira Monteiro Alves <danfma@gmail.com>
2017-08-07 14:11:43 -0300

    Updating Jest

---
+++ kodando-jest/src/main/kotlin/kodando/jest/Expect.kt
@@ -0,0 +1,100 @@
+package kodando.jest
+
+import kotlin.js.RegExp
+
+external interface Expect<in T> {
+    val not: Expect<T>
+
+    /**
+     * Uses === to test exact equality. If you want to check the value of an object, use toEqual instead.
+     */
+    fun toBe(value: T)
+
+    /**
+     * Recursively checks every field of an object or array.
+     */
+    fun toEqual(value: T)
+
+    // TRUTHINESS
+
+    /**
+     * Matches only null
+     */
+    fun toBeNull()
+
+    /**
+     * Matches only undefined.
+     */
+    fun toBeUndefined()
+
+    /**
+     * Is the opposite of toBeUndefined
+     */
+    fun toBeDefined()
+
+    /**
+     * Matches anything that an if statement treats as true
+     */
+    fun toBeTruthy()
+
+    /**
+     * Matches anything that an if statement treats as false
+     */
+    fun toBeFalsy()
+
+    /**
+     * Verifies if the value matches the specified pattern.
+     */
+    fun toMatch(pattern: RegExp)
+
+    /**
+     * Verifies if the value is greater than the specified one.
+     */
+    fun toBeGreaterThan(value: Number)
+
+    /**
+     * Verifies if the value is greater than or equal to the specified one.
+     */
+    fun toBeGreaterThanOrEqual(value: Number)
+
+    /**
+     * Verifies if the value is less than the specified one.
+     */
+    fun toBeLessThan(value: Number)
+
+    /**
+     * Verifies if the value is less or equal than the specified one.
+     */
+    fun toBeLessThanOrEqual(value: Number)
+
+    /**
+     * Check if an array contains a particular item
+     */
+    fun toContain(element: Any?)
+
+    /**
+     * Check if something has been thrown.
+     */
+    @JsName("toThrow")
+    fun toThrow()
+
+    /**
+     * Check if something has been thrown.
+     */
+    @JsName("toThrow")
+    fun toThrow(error: Throwable)
+
+    /**
+     * Check if something has been thrown.
+     */
+    @JsName("toThrow")
+    fun toThrow(errorMessage: String)
+
+    /**
+     * Check if something has been thrown.
+     */
+    @JsName("toThrow")
+    fun toThrow(errorPattern: RegExp)
+}
+
+external fun <T> expect(value: T): Expect<T>
--- kodando-jest/src/main/kotlin/kodando/jest/testing.kt
+++ kodando-jest/src/main/kotlin/kodando/jest/testing.kt
@@ -6,17 +6,6 @@ package kodando.jest
 
 external fun test(name: String, handler: () -> Unit)
 
-external fun <T> expect(value: T): Expect<T>
-
-external interface Expect<in T> {
-    val not: Expect<T>
-    fun toBe(value: T)
-    fun toBeNull()
-    fun toBeUndefined()
-    fun toBeTruthy()
-    fun toEqual(value: T)
-}
-
 external fun describe(name: String, handler: () -> Unit)
 
 @JsName("it")
