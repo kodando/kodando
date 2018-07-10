34cda49c56fbc95fca6a33852a4e87d065c02459
Daniel Ferreira Monteiro Alves <danfma@gmail.com>
2018-07-10 00:48:24 -0300

    chore(kodando): Updating publish script

--- publish-all.sh
+++ publish-all.sh
@@ -7,7 +7,7 @@ publish()
   ./gradlew -p $1 -PbintrayUser=${BINTRAY_USERNAME} -PbintrayKey=${BINTRAY_PASSKEY} bintrayUpload || echo "Could not publish $1"
 }
 
-for PROJECT in $(ls -d kodando-elmish*)
+for PROJECT in $(ls -d kodando-*)
 do
   publish "$PROJECT"
 done
