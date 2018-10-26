9b22a310d67857f26bc1f884e9bfe9206d91269f
Daniel Ferreira Monteiro Alves <dalves@daitangroup.com>
2018-10-26 15:22:30 -0300

    Migrating to Kotlin 1.3.0-rc

--- kodando-mobx/src/main/kotlin/kodando/mobx/AsyncActionPropertyProvider.kt
+++ kodando-mobx/src/main/kotlin/kodando/mobx/AsyncActionPropertyProvider.kt
@@ -1,8 +1,8 @@
 package kodando.mobx
 
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.startCoroutine
 import kotlin.js.Promise
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
@@ -14,15 +14,16 @@ class AsyncActionPropertyProvider<T, R>(private val action: SuspendableAsyncFunc
     val action = Mobx.action(actionName) { input: T ->
       Promise<R> { resolve, reject ->
         this.action.startCoroutine(input, completion = object : Continuation<R> {
+
           override val context: CoroutineContext = ActionCoroutineContext(actionName)
 
-          override fun resume(value: R) {
-            resolve(value)
+          override fun resumeWith(result: Result<R>) {
+            result.fold(
+              { resolve(it) },
+              { reject(it) }
+            )
           }
 
-          override fun resumeWithException(exception: Throwable) {
-            reject(exception)
-          }
         })
       }
     }
