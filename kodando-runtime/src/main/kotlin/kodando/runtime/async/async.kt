e4cb631491a3f87015401b58d1f073b449332e04
Daniel Ferreira Monteiro Alves <dalves@daitan.com>
2018-11-26 18:00:17 -0200

    fix(kodando-runtime): Trying to fix the async coroutine

--- kodando-runtime/src/main/kotlin/kodando/runtime/async/async.kt
+++ kodando-runtime/src/main/kotlin/kodando/runtime/async/async.kt
@@ -1,21 +1,23 @@
 package kodando.runtime.async
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.promise
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.js.Promise
 
 /**
  * Created by danfma on 17/01/2017.
  */
 
-fun <T> async(block: suspend () -> T): Promise<T> {
-  val scope = object : CoroutineScope {
-    override val coroutineContext: CoroutineContext = Job()
-  }
+private val asyncScope = object : CoroutineScope {
+  override val coroutineContext: CoroutineContext = EmptyCoroutineContext
+}
 
-  return scope.promise {
+fun <T> async(block: suspend () -> T): Promise<T> {
+  console.log("Before starting the coroutines")
+
+  return asyncScope.promise {
     block()
   }
 }
--- kodando-runtime/src/main/kotlin/kodando/runtime/async/await.kt
+++ kodando-runtime/src/main/kotlin/kodando/runtime/async/await.kt
@@ -3,4 +3,4 @@ package kodando.runtime.async
 import kotlinx.coroutines.await
 import kotlin.js.Promise
 
-suspend inline fun <T> await(promise: Promise<T>) = promise.await()
+suspend fun <T> await(promise: Promise<T>) = promise.await()
